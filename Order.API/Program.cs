7bc71083f2c59489bd3cc5011d7350977cf1ba05
tahapek5454 <tahapek5454@gmail.com>
2023-11-26 13:47:17 +0300

    feat: prepared minimal order api

--- Order.API/Program.cs
+++ Order.API/Program.cs
@@ -1,25 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.MapGet("/ready", () =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+    Console.WriteLine("Order service is ready");
 
-app.UseAuthorization();
+    return true;
 
-app.MapControllers();
+});
+
+app.MapGet("/commit", () =>
+{
+
+    Console.WriteLine("Order service is commited");
+
+    return true;
+
+});
+
+app.MapGet("/rollback", () =>
+{
+    Console.WriteLine("Order service is rollbacked");
+});
 
 app.Run();
